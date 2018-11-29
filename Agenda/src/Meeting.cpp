e475282cc4aa5084c2c89416c6e2e01da800b94a
LeonhardE <31626796+LeonhardE@users.noreply.github.com>
2018-11-29 18:14:09 +0800

    Update Meeting.cpp

--- Agenda/src/Meeting.cpp
+++ Agenda/src/Meeting.cpp
@@ -11,44 +11,44 @@
 using namespace std;
 
 Meeting::Meeting(const string &t_sponsor,
-	             const vector<string> &t_participator,
-	             const Date &t_startTime, const Date &t_endTime,
-	             const string &t_title)
+	         const vector<string> &t_participator,
+	         const Date &t_startTime, const Date &t_endTime,
+	         const string &t_title)
 {
-	m_sponsor = t_sponsor;
-	m_participators = t_participator;
-	m_startDate = t_startTime;
-	m_endDate = t_endTime;
-	m_title = t_title;
+    m_sponsor = t_sponsor;
+    m_participators = t_participator;
+    m_startDate = t_startTime;
+    m_endDate = t_endTime;
+    m_title = t_title;
 }
 
 Meeting::Meeting(const Meeting &t_meeting)
 {
-	m_sponsor = t_meeting.m_sponsor;
-	m_participators = t_meeting.m_participators;
-	m_startDate = t_meeting.m_startDate;
-	m_endDate = t_meeting.m_endDate;
-	m_title = t_meeting.m_title;
+    m_sponsor = t_meeting.m_sponsor;
+    m_participators = t_meeting.m_participators;
+    m_startDate = t_meeting.m_startDate;
+    m_endDate = t_meeting.m_endDate;
+    m_title = t_meeting.m_title;
 }
 
 string Meeting::getSponsor(void) const
 {
-	return m_sponsor;
+    return m_sponsor;
 }
 
 void Meeting::setSponsor(const string &t_sponsor)
 {
-	m_sponsor = t_sponsor;
+    m_sponsor = t_sponsor;
 }
 
 vector<string> Meeting::getParticipator(void) const
 {
-	return m_participators;
+    return m_participators;
 }
 
 void Meeting::setParticipator(const vector<string> &t_participator)
 {
-	m_participators = t_participator;
+    m_participators = t_participator;
 }
 
 
@@ -76,7 +76,7 @@ Date Meeting::getStartDate(void) const
 
 void Meeting::setStartDate(const Date &t_startTime)
 {
-	m_startDate = t_startTime;
+    m_startDate = t_startTime;
 }
 
 Date Meeting::getEndDate(void) const
@@ -86,17 +86,17 @@ Date Meeting::getEndDate(void) const
 
 void Meeting::setEndDate(const Date &t_endTime)
 {
-	m_endDate = t_endTime;
+    m_endDate = t_endTime;
 }
 
 string Meeting::getTitle(void) const
 {
-	return m_title;
+    return m_title;
 }
 
 void Meeting::setTitle(const string &t_title)
 {
-	m_title = t_title;
+    m_title = t_title;
 }
 
 bool Meeting::isParticipator(const string &t_username) const
